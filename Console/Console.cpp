f4d9a6c6aea1d2be5e98ecdb794f0a0771cc9a43
Raphael Bolliger <raphael.bolliger@students.fhnw.ch>
2017-01-11 11:16:27 +0100

    added vector class

--- Console/Console.cpp
+++ Console/Console.cpp
@@ -2,10 +2,18 @@
 //
 
 #include "stdafx.h"
+#include "Vector.h"
 
 
 int main()
 {
+
+	Vector<int, 4> A({1, 2, 3, 4});
+	cout << A;
+
+	Vector<int, 0> D;
+	cout << D;
+
     return 0;
 }
 
---
+++ Console/Vector.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <array>
+#include <iostream>
+
+using namespace std;
+
+template<class T, size_t S> class Vector : public array<T, S> {
+
+	public:
+
+	Vector() : array<T, S>() {
+
+	}
+
+	Vector(const initializer_list<T>& data) {
+		size_t s = __min(data.size(), S);
+		auto it = data.begin();
+		for (size_t i = 0; i < s; i++) this->at(i) = *it++;
+	}
+
+	friend ostream& operator<<(ostream& os, const Vector& v)
+	{
+		os << "[";
+		for (size_t i = 0; i < v.size(); i++)
+		{
+			if (i != 0) os << ",";
+			os << v.at(i);
+		}
+		os << "]" << endl;
+		return os;
+	}
+
+};
